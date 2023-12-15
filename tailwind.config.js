ff04f23d1e15219f342e07d93f75b256e751a730
sajaldas12 <sajaldas@inneed.cloud>
2023-12-15 14:30:03 +0600

    project-setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
